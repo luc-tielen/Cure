05daf8a34157ea00f374af2ad23c62a67116e16b
Luc Tielen <luc.tielen@gmail.com>
2017-05-02 20:50:11 +0200

    Rewrite of C code

--- c_src/elixir_comm.c
+++ c_src/elixir_comm.c
@@ -6,43 +6,39 @@
 #define STDIN  0
 #define STDOUT 1
 
+
 /*
  * Helper function to read data from Erlang/Elixir from stdin.
  * Returns the number of bytes read (-1 on error), fills buffer with data.
  */
-static int read_input(char* buffer, int length)
+static ssize_t read_input(char *buffer, uint16_t length)
 {
-    int bytes_read = read(STDIN, buffer, length);
-    if(bytes_read != length){
-        return -1;
-    }
-
+    ssize_t bytes_read = read(STDIN, buffer, length);
+    if (bytes_read != length) { return -1; }
     return bytes_read;
 }
 
-int read_msg(char* buffer)
+ssize_t read_msg(char *buffer)
 {
-    byte len[2]; //first 2 bytes contain length of the message.
-    int length;
+    uint8_t len[2];  // first 2 bytes contain length of the message.
 
-    if(read_input((char*)len, 2) != 2){
-        return -1;
-    }
+    if (read_input((char*) len, 2) != 2) { return -1; }
 
-    length = (len[0] << 8) | len[1];
+    uint16_t length = (len[0] << 8) | len[1];
     return read_input(buffer, length);
 }
 
-void send_msg(char* buffer, int length)
+void send_msg(char *buffer, uint16_t length)
 {
-    byte len[2]; //first 2 bytes contain length of the message.
+    uint8_t len[2];  // first 2 bytes contain length of the message.
     len[0] = (length >> 8) & 0xff;
     len[1] = length & 0xff;
     write(STDOUT, len, 2);
     write(STDOUT, buffer, length);
 }
 
-void send_error(char* error_message)
+void send_error(char *error_message)
 {
     send_msg(error_message, strlen(error_message));
 }
+
--- c_src/elixir_comm.h
+++ c_src/elixir_comm.h
@@ -1,5 +1,7 @@
 #ifndef ELIXIR_COMM_H
 #define ELIXIR_COMM_H
+#include <stdint.h>  // uint16_t
+#include <stdlib.h>  // ssize_t
 
 #ifdef __cplusplus
 extern "C" {
@@ -7,24 +9,23 @@ extern "C" {
 
 #define MAX_BUFFER_SIZE 65535
 
-typedef unsigned char byte;
 
 /*
  * Reads a message coming from Erlang/Elixir from stdin.
  * Returns the number of bytes read (-1 on error), fills the buffer with data.
  */
-int read_msg(char* buffer);
+ssize_t read_msg(char *buffer);
 
 /*
  * Sends a message to Erlang/Elixir via stdout.
  */
-void send_msg(char* buffer, int length);
+void send_msg(char *buffer, uint16_t length);
 
 /*
  * Helper function to send an error message back to Erlang/Elixir.
  * The message has to be a string that terminates with \0.
  */
-void send_error(char* error_message);
+void send_error(char *error_message);
 
 
 #ifdef __cplusplus
@@ -32,3 +33,4 @@ void send_error(char* error_message);
 #endif
 
 #endif
+
